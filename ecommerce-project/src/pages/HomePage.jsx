6be051f672d71c33128939ab47d3cdf5337c3fd9
shrijana thapa <shrijanathapa06@gmail.com>
2026-03-19 17:14:23 +0545

    use axios to fetch data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,15 +1,12 @@
+import axios from 'axios';
 import './HomePage.css';
 import { Header } from '../components/Header';
 import { products } from '../../starting-codes/data/products';
 
 export function HomePage() {
-  fetch('http://localhost:3000/api/products').then(
-    ((response) => {
-      return response.json();
-    }).then((data) => {
-      console.log(data);
-    }),
-  );
+  axios.get('http://localhost:3000/api/products').then((response) => {
+    return response.data;
+  });
   return (
     <>
       <title>Ecommerce Project</title>

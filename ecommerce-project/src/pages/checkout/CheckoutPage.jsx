15830c449f406e979a9a7d2a753769b08b40baec
shrijana thapa <shrijanathapa06@gmail.com>
2026-03-24 14:12:18 +0545

    create update delivery option

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -5,7 +5,7 @@ import './CheckoutPage.css';
 import { OrderSummary } from './orderSummary';
 import { PaymentSummary } from './paymentSummary';
 
-export function CheckoutPage({ cart }) {
+export function CheckoutPage({ cart, loadCart }) {
   const [deliveryOptions, setDeliveryOption] = useState([]);
   const [paymentSummary, setPaymentSummary] = useState(null);
 
@@ -19,7 +19,7 @@ export function CheckoutPage({ cart }) {
       setPaymentSummary(response.data);
     };
     fetchCheckoutData();
-  }, []);
+  }, [cart]);
   return (
     <>
       <title>Checkout</title>
@@ -51,7 +51,11 @@ export function CheckoutPage({ cart }) {
         <div className="page-title">Review your order</div>
 
         <div className="checkout-grid">
-          <OrderSummary deliveryOptions={deliveryOptions} cart={cart} />
+          <OrderSummary
+            deliveryOptions={deliveryOptions}
+            cart={cart}
+            loadCart={loadCart}
+          />
           <PaymentSummary paymentSummary={paymentSummary} />
         </div>
       </div>
--- ecommerce-project/src/pages/checkout/orderSummary.jsx
+++ ecommerce-project/src/pages/checkout/orderSummary.jsx
@@ -1,7 +1,8 @@
 import dayjs from 'dayjs';
 import { formatMoney } from '../../utils/money';
+import { DeliveryOption } from './deliveryOption';
 
-export function OrderSummary({ deliveryOptions, cart }) {
+export function OrderSummary({ deliveryOptions, cart, loadCart }) {
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 &&
@@ -44,39 +45,11 @@ export function OrderSummary({ deliveryOptions, cart }) {
                   </div>
                 </div>
 
-                <div className="delivery-options">
-                  <div className="delivery-options-title">
-                    Choose a delivery option:
-                  </div>
-                  {deliveryOptions.map((deliveryOption) => {
-                    let priceString = 'FREE Shipping';
-                    if (deliveryOption.priceCents > 0) {
-                      priceString = `${formatMoney(deliveryOption.priceCents)} -Shipping`;
-                    }
-                    return (
-                      <div key={deliveryOption.id} className="delivery-option">
-                        <input
-                          type="radio"
-                          checked={
-                            deliveryOption.id === cartItem.deliveryOptionId
-                          }
-                          className="delivery-option-input"
-                          name={`delivery-option-${cartItem.productId}`}
-                        />
-                        <div>
-                          <div className="delivery-option-date">
-                            {dayjs(
-                              deliveryOption.estimatedDeliveryTimeMs,
-                            ).format('dddd,MMMM,D')}
-                          </div>
-                          <div className="delivery-option-price">
-                            {priceString}
-                          </div>
-                        </div>
-                      </div>
-                    );
-                  })}
-                </div>
+                <DeliveryOption
+                  deliveryOptions={deliveryOptions}
+                  cartItem={cartItem}
+                  loadCart={loadCart}
+                />
               </div>
             </div>
           );
